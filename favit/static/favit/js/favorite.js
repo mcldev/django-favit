c26e68b752685c5a67b9f005bb94f32a4772c33e
carderm <michael.carder@michaelcarder.co.uk>
2018-01-19 15:24:12 +1100

    JS update to change button title

--- favit/static/favit/js/favorite.js
+++ favit/static/favit/js/favorite.js
@@ -1,58 +1,59 @@
-jQuery(document).ready(function($) {
-  $('.btn.favorite').click(function() {
-      var $obj = $(this);
-      var target_id = $obj.attr('id').split('_')[1];
+jQuery(document).ready(function ($) {
+    $('.btn.favorite').click(function () {
+        var $obj = $(this);
+        var target_id = $obj.attr('id').split('_')[1];
 
-      var add_fav_icon = $obj.attr('data-add-fav-icon');
-      if (typeof add_fav_icon === typeof undefined || add_fav_icon === false) {
-          add_fav_icon = "fa-heart-o";
-      }
+        var add_fav_icon = $obj.attr('data-add-fav-icon') || "fa-heart-o";
+        var remove_fav_icon = $obj.attr('data-remove-fav-icon') || "fa-heart";
 
-      var remove_fav_icon = $obj.attr('data-remove-fav-icon');
-      if (typeof remove_fav_icon === typeof undefined || remove_fav_icon === false) {
-          remove_fav_icon = "fa-heart";
-      }
+        var add_fav_title = $obj.attr('data-add-fav-title') || "Add to Favourites";
+        var remove_fav_title = $obj.attr('data-remove-fav-title') || "Remove from Favourites";
 
-
-      $obj.prop('disabled', true);
-      $.ajax({
-          url: $obj.attr('href'),
-          type: 'POST',
-          data: {
-              target_model: $obj.attr('model'),
-              target_object_id: target_id
-          },
-          success: function (response) {
-              if (response.status == 'added') {
-                  $obj.children().removeClass(add_fav_icon).addClass(remove_fav_icon);
-              }
-              else {
-                  $obj.children().removeClass(remove_fav_icon).addClass(add_fav_icon);
-              }
-              //$obj.parent('.favit').children('.fav-count').text(response.fav_count);
-              $obj.prop('disabled', false);
-          }
-      });
-  });
-
-  $('.btn.unfave').click(function() {
-    var $obj = $(this);
-    $obj.prop('disabled', true);
-    $.ajax({
-      url: $obj.attr('href'),
-      type: 'POST',
-      data: {
-        target_model: $obj.data('model'),
-        target_object_id: $obj.data('id')
-      },
-      success: function(response) {
-        if (response.status == 'deleted') {
-          $obj.parent().remove();
-        }
-      },
-      complete: function(response) {
-        $obj.prop('disabled', false);
-      }
+        $obj.prop('disabled', true);
+        $.ajax({
+            url: $obj.attr('href'),
+            type: 'POST',
+            data: {
+                target_model: $obj.attr('model'),
+                target_object_id: target_id
+            },
+            success: function (response) {
+                if (response.status == 'added') {
+                    $obj.children().removeClass(add_fav_icon).addClass(remove_fav_icon);
+                    if ($obj.attr('title') && $obj.title === add_fav_title) {
+                        $obj.title = remove_fav_title;
+                    }
+                }
+                else {
+                    $obj.children().removeClass(remove_fav_icon).addClass(add_fav_icon);
+                    if ($obj.attr('title') && $obj.title === remove_fav_title) {
+                        $obj.title = add_fav_title;
+                    }
+                }
+                //$obj.parent('.favit').children('.fav-count').text(response.fav_count);
+                $obj.prop('disabled', false);
+            }
+        });
     });
-  });
+
+    $('.btn.unfave').click(function () {
+        var $obj = $(this);
+        $obj.prop('disabled', true);
+        $.ajax({
+            url: $obj.attr('href'),
+            type: 'POST',
+            data: {
+                target_model: $obj.data('model'),
+                target_object_id: $obj.data('id')
+            },
+            success: function (response) {
+                if (response.status == 'deleted') {
+                    $obj.parent().remove();
+                }
+            },
+            complete: function (response) {
+                $obj.prop('disabled', false);
+            }
+        });
+    });
 });
