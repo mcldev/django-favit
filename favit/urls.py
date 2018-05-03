4480f2e541fc7ad7fc5bf41fadb824dd2f7b8767
carderm <michael.carder@michaelcarder.co.uk>
2018-05-03 15:42:26 +1000

    Added named views to the urls.py

--- favit/urls.py
+++ favit/urls.py
@@ -3,6 +3,6 @@ from favit.views import *
 
 
 urlpatterns = [
-    url(r'^add-or-remove$', add_or_remove),
-    url(r'^remove$', remove),
+    url(r'^add-or-remove$', add_or_remove, name='favit.views.add_or_remove'),
+    url(r'^remove$', remove, name='favit.views.remove'),
 ]
